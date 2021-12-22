3f2f012e15d2339371d3aa109c156eab61017473
Manoj <manojmodugala@gmail.com>
2021-12-22 23:04:29 +0530

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse()
+}
+
 const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts')
@@ -13,6 +17,12 @@ app.use('/', indexRoute)
 
 
 
+//Database related
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection;
+db.on('error',(error) => econsole.error(error))
+db.once('open',() => console.log('Connected to mongoose database'))
 
 app.listen(process.env.PORT || 3000);
 
